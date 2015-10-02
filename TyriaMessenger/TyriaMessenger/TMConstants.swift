ccd637be296715a97f3c1a9aadf3a597d0c3e6fb
Christian Grimsgaard <chisse1049@hotmail.com>
2015-10-02 13:41:25 +0200

    Added Constants file

---
+++ TyriaMessenger/TyriaMessenger/TMConstants.swift
@@ -0,0 +1,29 @@
+//
+//  TMConstants.swift
+//  TyriaMessenger
+//
+//  Created by Christian Grimsgaard on 02/10/15.
+//  Copyright © 2015 ChristianGrimsgaard. All rights reserved.
+//
+
+// Tyria Messenger
+let TM_APP_TITLE                        = "Tyria Messenger"
+
+// Official Guild Wars 2 API End-points
+let TM_GW2API_V1_BASE_URL               = "https://api.guildwars2.com/v1/"
+let TM_GW2API_V2_BASE_URL               = "https://api.guildwars2.com/v2/"
+
+let TM_GW2API_TOKENINFO_UR              = TM_GW2API_V2_BASE_URL + "tokeninfo"
+let TM_GW2API_ACCOUNT_URL               = TM_GW2API_V2_BASE_URL + "account"
+
+let TM_GW2API_GUILD_BASE_URL            = TM_GW2API_V1_BASE_URL + "guild_details.json"
+let TM_GW2API_GUILD_DETAILS_URL         = TM_GW2API_GUILD_BASE_URL + "?guild_id=$GUILD_ID"
+
+//let TM_GW2API_GUILD_BASE_URL            = TM_GW2API_V2_BASE_URL + "guild/"
+//let TM_GW2API_GUILD_DETAILS_URL         = TM_GW2API_GUILD_BASE_URL + "$GUILD_ID"
+//let TM_GW2API_GUILD_INVENTORY_URL       = TM_GW2API_GUILD_BASE_URL + "$GUILD_ID/inventory"
+//let TM_GW2API_GUILD_LOG_URL             = TM_GW2API_GUILD_BASE_URL + "$GUILD_ID/log"
+//let TM_GW2API_GUILD_MEMBERS_URL         = TM_GW2API_GUILD_BASE_URL + "$GUILD_ID/members"
+//let TM_GW2API_GUILD_RANKS_URL           = TM_GW2API_GUILD_BASE_URL + "$GUILD_ID/ranks"
+//let TM_GW2API_GUILD_PERMISSIONS_URL     = TM_GW2API_GUILD_BASE_URL + "permissions"
+//let TM_GW2API_GUILD_UPGRADES_URL        = TM_GW2API_GUILD_BASE_URL + "upgrades"
